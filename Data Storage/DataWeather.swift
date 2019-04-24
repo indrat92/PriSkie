890e02605a3efd4a871ce31be08843327ce148e1
indratdev <indrat.dev@gmail.com>
2019-04-24 10:32:36 +0700

    Base Project

---
+++ Data Storage/DataWeather.swift
@@ -0,0 +1,22 @@
+//
+//  DataWeather.swift
+//  PriSkie
+//
+//  Created by IndratS on 23/04/19.
+//  Copyright © 2019 IndratS. All rights reserved.
+//
+
+import Foundation
+
+
+class DataWeather{
+    
+    
+    var long: String = ""
+    var lat: String = ""
+    let keyID: String = "fd90a5793222ed95e3566bc35a5efc26"
+    let urlWeather:String = "https://api.darksky.net/forecast"
+    
+    
+    
+}
